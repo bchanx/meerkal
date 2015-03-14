c1551db8e23cf8b107bacfa35e4bb74556fb4473
Matthaeus <mattkrenn@gmail.com>
2015-03-14 02:04:50 -0700

    do the party dog

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,13 +7,13 @@ var now = Date.now();
 $(function () { 
 
     $('#liveButton').bind('click', function(e){
-        $('div.live').css('visibility', 'visible');
-        $('div.schedule').css('visibility', 'hidden');
+        $('div.live').css('display', 'block');
+        $('div.schedule').css('display', 'none');
     });
 
     $('#comingButton').bind('click', function(e){
-        $('div.live').css('visibility', 'hidden');
-        $('div.schedule').css('visibility', 'visible');
+        $('div.live').css('display', 'none');
+        $('div.schedule').css('display', 'block');
     });
 
 
@@ -26,28 +26,36 @@ $(function () {
 
 
 
+
+
 function loadTweets() {
 
-    $.getJSON( "api/twitter/recent", function( data ) {
-        // data.data;
+    $.ajax({
+        url: 'http://whatsonmeerkat.com/api/twitter/recent',
+        jsonp: 'callback',
+        dataType: 'jsonp',
+        success: function( data ) {
 
-        var i = 0;
-        for (var i in data.data) {
+            var i = 0;
+            for (var i in data.data) {
 
-            var streamItem = data.data[i];
+                var streamItem = data.data[i];
 
-            streamItem.created_at = Date.parse(streamItem.created_at);
-            tweets.push(streamItem);
-            addStreamItemToHtml(streamItem, i);
+                streamItem.created_at = Date.parse(streamItem.created_at);
+                tweets.push(streamItem);
+                addStreamItemToHtml(streamItem, i);
+            }
+
         }
-
-        // console.log(data.data[0]); 
-
-        // var tweetTS = Date.parse(data.data[0].created_at);
-
-        // console.log(hoursMinutesSecondsString(Math.round((now - tweetTS)/1000), true));
-
     });
+
+    console.log('22222');
+
+
+    // $.getJSON('http://whatsonmeerkat.com/api/twitter/recent?callback=abc', 
+    //     {format: 'jsonp'}
+    // );
+
 
 }
 
@@ -57,16 +65,22 @@ function loadTweets() {
 
 function addStreamItemToHtml(streamItem, index) {
 
-    console.log(streamItem);
+    // console.log(streamItem, 'sdfdsfdsf');
     
     var outputString = '';
     var timeDiff = Math.round((now - streamItem.created_at)/1000);
     var profileImgURL = streamItem.user.profile_image_url;
 
+    outputString += '<a href="'+ streamItem.entities.urls[0].expanded_url +'">';
     outputString += '<div class="oneStream">';
     outputString += '<span class="time" id="time'+ index +'">'+ hMSString(timeDiff, false) +'</span>';
     outputString += '<img class="twitterProfileImage" src="'+ profileImgURL +'" />';
+    // outputString += '<span class="name">'+ streamItem.user.name +'</span>';
+    outputString += '<span class="text"><b>' + streamItem.user.name + '</b> ' + streamItem.text +'</span>';
     outputString += '</div>';
+    outputString += '</a>';
+
+    $(outputString).appendTo('div.live');
 
 }
 
