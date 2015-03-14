3b952eb0b6b678372a9c69a9a58e600d652a0a80
bchanx <bchanx@gmail.com>
2015-03-13 18:02:51 -0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var order = require('gulp-order');
+var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var stylus = require('gulp-stylus');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var spawn = require('child_process').spawn;
+
+gulp.task('nodemon', function() {
+  return nodemon({
+    script: 'app.js'
+  });
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync({
+    proxy: 'http://localhost:7000',
+    files: ['routes/*.js', 'public/**/*.*', 'views/**/*.*'],
+    port: 3000
+  });
+  gulp.watch('stylesheets/**/*.styl', ['styl']);
+  gulp.watch('stylesheets/**/*.css', ['css']);
+  gulp.watch('scripts/**/*.js', ['js']);
+});
+
+gulp.task('styl', function() {
+  gulp.src('stylesheets/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('stylesheets/stylus'))
+});
+
+gulp.task('css', function() {
+  // Library css
+  gulp.src('stylesheets/lib/*.css')
+    .pipe(order([
+      'normalize.css'
+    ]))
+    .pipe(concat('deps.css'))
+    .pipe(gulp.dest('public/stylesheets'));
+
+  // Dev css
+  gulp.src(['stylesheets/*.css', 'stylesheets/stylus/*.css'])
+    .pipe(order([
+      'style.css',
+      'test.css',
+      '*.css'
+    ]))
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(cssmin())
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('js', function() {
+  // Library scripts
+  gulp.src('scripts/lib/*.js')
+    .pipe(order([
+      'jquery.js'
+    ]))
+    .pipe(concat('deps.js'))
+    .pipe(gulp.dest('public/scripts'));
+
+  // Dev scripts
+  gulp.src('scripts/*.js')
+    .pipe(order([
+      'test.js'
+    ]))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/scripts'))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('start', ['css', 'js', 'browser-sync'], function() {
+  // start
+});
+
+gulp.task('default', function() {
+  var process;
+  function restart() {
+    if (process) {
+      process.kill();
+    }
+    process = spawn('gulp', ['start'], {stdio: 'inherit'});
+  }
+
+  gulp.watch('gulpfile.js', restart);
+  restart();
+});
---
+++ setup.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+###
+### G L O B A L S
+###
+SOURCE=~/.bashrc
+
+###
+### L O G G I N G
+###
+function log {
+  echo -e "\033[1;32m--- $@ ---\033[0m"
+}
+function err {
+  echo -e "\033[1;31m--- $@ ---\033[0m"
+}
+
+###
+### U T I L S
+###
+function ensureExists {
+  if [[ ! -e $1 ]]; then
+    touch $1
+  fi
+}
+function sourceIfExists {
+  if [[ -e $1 ]]; then
+    source $1
+  fi
+}
+function addBash {
+  ensureExists ~/.bashrc
+  if ! grep -q "$@" ~/.bashrc; then
+    echo "$@" >> ~/.bashrc 2>&1
+  fi
+}
+function addPath {
+  if [[ ! $(echo $PATH 2>&1) =~ $1 ]]; then
+    addBash "export PATH=$1:\$PATH"
+  fi
+  sourceIfExists $SOURCE
+}
+
+###
+### S E T U P
+###
+function setupBashProfile {
+  SOURCE=~/.bash_profile
+  ensureExists ~/.bash_profile
+  if ! grep -q "source ~/.bashrc" ~/.bash_profile; then
+    echo "if [ -f ~/.bashrc ]; then
+  source ~/.bashrc
+fi" >> ~/.bash_profile
+  fi
+  log "[SETUP] .bash_profile"
+}
+function setupHomebrew {
+  BREW_VER=$(brew --version 2>&1)
+  if [[ $BREW_VER =~ "command not found" ]]; then
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+  fi
+  addPath /usr/local/bin
+  BREW_DOC=$(brew doctor 2>&1)
+  if [[ ! $BREW_DOC =~ "Your system is ready to brew." ]]; then
+    if [[ $BREW_DOC =~ "Your Homebrew is outdated." ]]; then
+      brew update  
+    else
+      err "Run \`brew doctor\` and fix your system before continuing."
+      err "If it's a path related issue, consider modifying /etc/paths."
+      exit 1
+    fi
+  fi
+  log "[SETUP] Homebrew $BREW_VER"
+}
+function setupGit {
+  # Git/Xcode/GCC
+  GIT_VER=$(git --version 2>&1)
+  if [[ ! $GIT_VER =~ "git version" ]]; then
+    err "Download and install Xcode, then rerun this script."
+    exit 1
+  fi
+  if [[ ! $(git config --get push.default 2>&1) =~ "simple" ]]; then
+    git config --global push.default simple
+  fi
+  log "[SETUP] $GIT_VER"
+}
+function setupNode {
+  # Node & npm
+  brew install node
+  addPath /usr/local/share/npm/bin
+  NPM_VER=$(npm --version 2>&1)
+  if [[ $NPM_VER =~ "command not found" ]]; then
+    wget -qO- https://npmjs.org/install.sh | sh
+  fi
+  if [[ $(lessc --version 2>&1) =~ "command not found" ]]; then
+    npm install -g less@1.5.1
+    npm install -g bower
+    npm install -g gulp
+  fi
+  log "[SETUP] Node $NPM_VER"
+}
+function setupSSH {
+  if [[ ! -d ~/.ssh ]]; then
+    pushd . > /dev/null
+    cd ~
+    ssh-keygen
+    popd > /dev/null
+  fi
+  read -p "Make sure you have SSH keys added to Github (press enter to continue)"
+}
+
+SYSTEM=$(uname)
+if [ $SYSTEM = "Darwin" ]; then
+  ###
+  ### M A C  O S
+  ###
+  setupBashProfile
+  setupHomebrew
+  setupGit
+  setupNode
+  setupSSH
+fi
+
